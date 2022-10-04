e0e9352e6ce2b0cd0fb66b6cac8223bb7b29c9ff
Mustafa Mohamed <mustafa.mohamed@kcl.ac.uk>
2022-10-04 16:02:01 +0100

    Exercise 1.4 complete

--- primes.py
+++ primes.py
@@ -1,6 +1,31 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("Incorrect value entered")
+    
+    
+
     list = []
+    i = 1
+    while len(list) < number_of_primes:
+        factorList = []
+        for n in range (1, i+1):
+            if i % n == 0:
+                    factorList.append(n)
+            elif len(factorList) > 2 :
+                break
+        if len(factorList) == 2:
+            list.append(i)        
+        i += 1
+
+        
+
+    
+
     return list
+
+
+
+        
+
+print(primes(10))
+
